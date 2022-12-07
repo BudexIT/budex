7c9b6a98e2c35402f59ce902c634229299f0d7f2
Patryk725 <ziomski725@gmail.com>
2022-12-07 12:33:34 +0100

    added

---
+++ informatyka/programowanie_aplikacji_mobilnych/2.0-instrukcje_zadania/zadanie_25.php
@@ -0,0 +1,9 @@
+<!-- 25. Program który sprawdzi czy wpisano tekst "24-100 Puławy"(if) -->
+
+<?php
+    $adres = "24-100 Puławy"
+
+    if ($adres == "24-100 Puławy") {
+        echo "adres to " . $adres;
+    }
+?>
